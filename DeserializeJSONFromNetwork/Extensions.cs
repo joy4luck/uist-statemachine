c6ea4729a765d90bc4e52e8e4feb6f0e104144f1
Geza Kovacs <gkovacs@mit.edu>
2012-09-04 16:14:26 -0400

    added missing file (extensions.cs)

---
+++ DeserializeJSONFromNetwork/Extensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeserializeJSONFromNetwork
+{
+    public static class Extensions
+    {
+        public static string PrintArray<T>(this T[] arr)
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append("[");
+            output.Append(String.Join(",", arr));
+            output.Append("]");
+            return output.ToString();
+        }
+    }
+}
